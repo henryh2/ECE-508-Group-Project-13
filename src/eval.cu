92d6e97555fa33fbd49ab4319bc80abed82e5523
ZongjingChen <zc55@illinois.edu>
2023-05-07 13:55:32 -0500

    Use triangle counting lab dataset, add comparison of performance

--- src/eval.cu
+++ src/eval.cu
@@ -81,12 +81,27 @@ static int eval(int iterations, const std::string& edge_path, const std::string&
 {
   pangolin::logger::set_level(pangolin::logger::Level::TRACE);
 
-  pangolin::EdgeList edgeList = import_edgelist(edge_path);
+  timer_start("Reading graph data");
+  // create a reader for .bel files
+  pangolin::BELReader reader(edge_path);
+  // read all edges from the file
+  pangolin::EdgeList edgeList = reader.read_all();
+  timer_stop(); // reading graph data
 
-  // Built the graph in CSR format
-  timer_start("Building unified memory CSR");
-  pangolin::COO<uint32_t> coo = pangolin::COO<uint32_t>::from_edgelist(edgeList);
-  timer_stop();
+  timer_start("building unified memory CSR");
+  // build a csr/coo matrix from the edge list
+  pangolin::COO<uint32_t> coo = pangolin::COO<uint32_t>::from_edgelist(
+    edgeList,
+    [](const pangolin::Edge &e) {return e.first <= e.second; } // keep src > dst
+  );
+  timer_stop(); // building unified memory CSR
+
+  // pangolin::EdgeList edgeList = import_edgelist(edge_path);
+
+  // // Built the graph in CSR format
+  // timer_start("Building unified memory CSR");
+  // pangolin::COO<uint32_t> coo = pangolin::COO<uint32_t>::from_edgelist(edgeList);
+  // timer_stop();
 
   // Imports the graph
   // timer_start("Reading graph data");
@@ -112,48 +127,62 @@ static int eval(int iterations, const std::string& edge_path, const std::string&
       break;
     }
     case GPU_LCC:
-      timer_start("Reading graph data");
-      std::vector<float> expected_lcc = import_solution_LCC(sol_path);
-      timer_stop();
-      // Compute the LCC
-      timer_start("Performing LCC");
-      std::vector<float> actual_lcc;
-
+      // timer_start("Reading graph data");
+      // std::vector<float> expected_lcc = import_solution_LCC(sol_path);
+      // timer_stop();
+      // // Compute the LCC
+      // timer_start("Performing LCC");
+      
+      std::vector<float> actual_lcc_baseline;
+      std::vector<float> actual_lcc_dynamic_parallel;
+      printf("--------------------------Performance------------------------\n");
       printf("num_rows: %u\n", coo.num_rows());
 
-      printf("Row index:\n");
-      for(int i = 0; i < coo.nnz(); i++) {
-        printf("%d ", coo.row_ind()[i]);
-      }
+      // printf("Row index:\n");
+      // for(int i = 0; i < coo.nnz(); i++) {
+      //   printf("%d ", coo.row_ind()[i]);
+      // }
+      // printf("\nCol index:\n");
+      // for(int i = 0; i < coo.nnz(); i++) {
+      //   printf("%d ", coo.col_ind()[i]);
+      // }
+      // printf("\nRow ptr:\n");
+      // for(int i = 0; i < 10; i++) {
+      //   printf("%d ", coo.row_ptr()[i]);
+      // }
 
-      printf("\nCol index:\n");
-      for(int i = 0; i < coo.nnz(); i++) {
-        printf("%d ", coo.col_ind()[i]);
-      }
-
-      printf("\nRow ptr:\n");
-      for(int i = 0; i < 10; i++) {
-        printf("%d ", coo.row_ptr()[i]);
-      }
+      timer_start("Expected LCC sequential");
+      pangolin::Vector<double> expected_lcc = count_triangles(coo.view(), 0);
+      timer_stop();
 
       // printf("num rows: %d\n", coo.num_rows());
-      // actual_lcc = LCC(coo.view(), coo.num_rows());
-      actual_lcc = LCC(coo.view(), 11);
-
-      // actual_lcc = LCC(coo.view(), 11);
-      // actual_lcc = LCC(coo.view());
-
-      printf("\nActual LCC: \n");
-      for(int i = 0; i < actual_lcc.size(); i++) {
-        printf("%d: %f\n", i, actual_lcc[i]);
-      }
-
-      // Validate the solution to the algorithm
-      timer_start("Validating LCC");
-      REQUIRE(expected_lcc.size() == actual_lcc.size());
-      for(uint32_t i = 1; i < expected_lcc.size(); ++i)
-        REQUIRE(expected_lcc[i] == actual_lcc[i]);
+      timer_start("Actual LCC baseline kernel");
+      actual_lcc_baseline = LCC(coo.view(), coo.num_rows());
       timer_stop();
+
+      timer_start("Actual LCC dynamic parallelism kernel");
+      // actual_lcc_dynamic_parallel = LCC_dynamic(coo.view(), coo.num_rows());
+      timer_stop;
+
+      // printf("\nActual LCC: \n");
+      // for(int i = 0; i < actual_lcc.size(); i++) {
+      //   printf("%d: %f\n", i, actual_lcc[i]);
+      // }
+      printf("-------------------------------------------------------------\n");
+      // Validate baseline solution
+      timer_start("Validating baseline LCC");
+      REQUIRE(expected_lcc.size() == actual_lcc_baseline.size());
+      for(uint32_t i = 0; i < expected_lcc.size(); ++i)
+        REQUIRE((float)expected_lcc[i] == actual_lcc_baseline[i]);
+      timer_stop();
+      printf("\n");
+
+      // Validate dynamic parallel solution
+      // timer_start("Validating dynamic parallel LCC");
+      // REQUIRE(expected_lcc.size() == actual_lcc_dynamic_parallel.size());
+      // for(uint32_t i = 0; i < expected_lcc.size(); ++i)
+      //   REQUIRE((float)expected_lcc[i] == actual_lcc_dynamic_parallel[i]);
+      // timer_stop();
   }
   
   // timer_stop();
@@ -193,7 +222,9 @@ TEST_CASE("directed-test", "")
 TEST_CASE("undirected-test", "") 
 {
   // SECTION("CDLP") { eval(2, GRAPH_PREFIX_PATH "/test-cdlp-undirected.e", GRAPH_PREFIX_PATH "/test-cdlp-undirected-CDLP", GPU_CDLP); }
-  SECTION("LCC") { eval(0, GRAPH_PREFIX_PATH "/test-lcc-undirected.e", GRAPH_PREFIX_PATH "/test-lcc-undirected-LCC", GPU_LCC); }
+  SECTION("LCC") { eval(0, GRAPH_PREFIX_PATH "/graph500-scale18-ef16_adj.bel", GRAPH_PREFIX_PATH "/test-lcc-undirected-LCC", GPU_LCC); }
+  SECTION("LCC") { eval(0, GRAPH_PREFIX_PATH "/amazon0302_adj.bel", GRAPH_PREFIX_PATH "/test-lcc-undirected-LCC", GPU_LCC); }
+  SECTION("LCC") { eval(0, GRAPH_PREFIX_PATH "/roadNet-CA_adj.bel", GRAPH_PREFIX_PATH "/test-lcc-undirected-LCC", GPU_LCC); }
 }
 
 } // namespace gpu_algorithms_labs_evaluation
--- src/lcc.cu
+++ src/lcc.cu
@@ -114,7 +114,7 @@ __device__ static uint32_t linear_search_and_add(const uint32_t *const edgeDst, 
 }
 
 
-__global__ static void triangle_count_kernel(uint32_t *__restrict__ triangleCounts, //!< per-node triangle counts
+__global__ static void triangle_count_dynamic_parallel_kernel(uint32_t *__restrict__ triangleCounts, //!< per-node triangle counts
                                  uint32_t *__restrict__ nodeCounts,
                                  const uint32_t *const edgeSrc,         //!< node ids for edge srcs
                                  const uint32_t *const edgeDst,         //!< node ids for edge dsts
@@ -194,6 +194,58 @@ __global__ static void triangle_count_kernel(uint32_t *__restrict__ triangleCoun
 }
 
 
+__global__ static void triangle_count_kernel(uint32_t *__restrict__ triangleCounts, //!< per-node triangle counts
+                                 uint32_t *__restrict__ nodeCounts,
+                                 const uint32_t *const edgeSrc,         //!< node ids for edge srcs
+                                 const uint32_t *const edgeDst,         //!< node ids for edge dsts
+                                 const uint32_t *const rowPtr,          //!< source node offsets in edgeDst
+                                 const size_t numEdges                  //!< how many edges to count triangles for
+) {
+  int tx = blockIdx.x * blockDim.x + threadIdx.x;
+  // The source node number map to array index
+  if(tx < numEdges) {
+    int nodeNum = edgeSrc[tx];
+    int dstNode = edgeDst[tx];
+    uint32_t uPtr = rowPtr[nodeNum];
+    uint32_t uEnd = rowPtr[nodeNum + 1];
+    uint32_t vPtr = rowPtr[edgeDst[tx]];
+    uint32_t vEnd = rowPtr[edgeDst[tx] + 1];
+
+    uint32_t uDiff = uEnd - uPtr;
+    uint32_t vDiff = vEnd - vPtr;
+    uint32_t x = 0;
+    // From triangle counting lab
+    // using binary search when V was as least 64 and V/U was at least 6 (V is the longer list length, and U the shorter one).
+    if (uDiff > vDiff && uDiff >= 64 && uDiff / vDiff >= 6) {
+      // One node may have many edges, use atomic add
+      x = binary_search_and_add(edgeDst, triangleCounts, vPtr, vEnd, uPtr, uEnd);
+      // atomicAdd(&triangleCounts[nodeNum], x);
+    }
+    else if(vDiff > uDiff && vDiff >= 64 && vDiff / uDiff >= 6) {
+      x = binary_search_and_add(edgeDst, triangleCounts, vPtr, vEnd, uPtr, uEnd);
+      // atomicAdd(&triangleCounts[nodeNum], x);
+    }
+    else{
+      x = linear_search_and_add(edgeDst, triangleCounts, vPtr, vEnd, uPtr, uEnd);
+      // atomicAdd(&triangleCounts[nodeNum], x);
+    }
+    // x = binary_search_and_add(edgeDst, triangleCounts, vPtr, vEnd, uPtr, uEnd);
+    // x = linear_search_and_add(edgeDst, triangleCounts, vPtr, vEnd, uPtr, uEnd);
+    atomicAdd(&triangleCounts[nodeNum], x);
+    atomicAdd(&triangleCounts[dstNode], x);
+    atomicAdd(&nodeCounts[nodeNum], 1);
+    atomicAdd(&nodeCounts[dstNode], 1);
+
+    // printf("Thread: %u, count: %u\n", tx, x);
+    // printf("Src node: %d\n", nodeNum);
+    // printf("Dst node: %d\n", dstNode);
+
+  }
+
+  __syncthreads();
+}
+
+
 __global__ static void coefficients_calculate_kernel(uint32_t *__restrict__ triangleCounts,     // per-node triangle counts
                                                     uint32_t *__restrict__ nodeCounts,
                                                     const uint32_t *const rowPtr,               // source node offsets in edgeDst
@@ -248,7 +300,45 @@ std::vector<float> LCC(const pangolin::COOView<uint32_t> view, uint32_t numNodes
   cudaFree(triangleCounts);
 
   std::vector<float> coef;
-  for(int i = 1; i < numNodes; ++i) {
+  for(int i = 0; i < numNodes; ++i) {
+    // printf("%d: %f\n", i, coefficients[i]);
+    coef.push_back(coefficients[i]);
+  }
+  free(coefficients);
+  return coef;
+}
+
+std::vector<float> LCC_dynamic(const pangolin::COOView<uint32_t> view, uint32_t numNodes) 
+{
+  dim3 dimBlock(BLOCK_SIZE);
+  // calculate the number of blocks needed
+  dim3 dimGridCount(ceil(view.nnz() * 1.0 / BLOCK_SIZE));
+  // Store triangle counts for each node 
+  uint32_t *triangleCounts;
+  uint32_t *nodeCounts;
+  cudaMalloc((void**) (&triangleCounts), numNodes * sizeof(uint32_t));
+  cudaMalloc((void**) (&nodeCounts), numNodes * sizeof(uint32_t));
+  float* coefficients = (float*)malloc(numNodes * sizeof(float));
+  
+  // Kernel coefficients array
+  float *kernel_coefficents;
+  cudaMalloc((void**) (&kernel_coefficents), numNodes * sizeof(float));
+  // triangle_count_kernel<<<dimGridCount, dimBlock>>>(triangleCounts, view.row_ind(), view.col_ind(), view.row_ptr(), view.nnz());
+  triangle_count_kernel<<<dimGridCount, dimBlock>>>(triangleCounts, nodeCounts, view.row_ind(), view.col_ind(), view.row_ptr(), view.nnz());
+
+  // One thread calculate the coefficient for one node
+  dim3 dimGridCoefficient(ceil(numNodes * 1.0 / BLOCK_SIZE));
+  // launch another kernal to compute lcc
+  // coefficients_calculate_kernel<<<dimGridCoefficient, dimBlock>>>(triangleCounts, view.row_ptr(), kernel_coefficents, numNodes);
+  coefficients_calculate_kernel<<<dimGridCoefficient, dimBlock>>>(triangleCounts, nodeCounts, view.row_ptr(), kernel_coefficents, numNodes);
+
+  cudaMemcpy(coefficients, kernel_coefficents, sizeof(float) * numNodes, cudaMemcpyDeviceToHost); 
+
+  cudaFree(kernel_coefficents);
+  cudaFree(triangleCounts);
+
+  std::vector<float> coef;
+  for(int i = 0; i < numNodes; ++i) {
     // printf("%d: %f\n", i, coefficients[i]);
     coef.push_back(coefficients[i]);
   }
